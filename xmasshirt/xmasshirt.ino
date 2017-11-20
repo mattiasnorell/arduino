2777aa946d386d258e863b6e4bfc40d38027258b
Mattias Norell <mattias.norell@gmail.com>
2017-11-20 17:05:59 +0100

    Xmas shirt - Added pattern support

--- xmasshirt/xmasshirt.ino
+++ xmasshirt/xmasshirt.ino
@@ -1,21 +1,62 @@
-const int numberOfLights = 4;                         // Total number of leds
-int loopSpeed = 1000;                                 // Loop speed in milliseconds
+const int SPEED_BUTTON_PIN = 1;
+const int PATTERN_BUTTON_PIN = 2;
+const int LED_PINS[4] = {3, 4, 5, 6};
+
+int speedIndex = 0;
+int sequencerSpeed[3] = {500, 1000, 2000};
 int sequenceIndex = 0;                                // Current sequence index
-int sequencerData[1][numberOfLights] = {0, 1, 0, 0};  // Led sequencer script
+int sequencerData[4][4][4] =                          // Max 4 different patterns per sequence
+{
+  {
+    {1, 0, 0, 0},
+    {0, 1, 0, 0},
+    {0, 0, 1, 0},
+    {0, 0, 0, 1}
+  }, {
+    {0, 1, 0, 1},
+    {1, 0, 1, 0},
+    {0, 1, 0, 1},
+    {1, 0, 1, 0}
+  }, {
+    {1, 0, 0, 1},
+    {0, 1, 1, 0},
+    {1, 0, 0, 1},
+    {0, 1, 1, 0}
+  }, {
+    {1, 1, 0, 1},
+    {0, 1, 1, 0},
+    {1, 0, 1, 1},
+    {1, 1, 0, 1}
+  }
+};
 
 void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+  }
 
+  pinMode(PATTERN_BUTTON_PIN, INPUT);
+  pinMode(SPEED_BUTTON_PIN, INPUT);
 }
 
 void loop() {
   sequencer();
-  delay(loopSpeed);
+  
+  delay(sequencerSpeed[speedIndex]);
+}
+
+void changeSpeed() {
+  speedIndex++;
+
+  if (speedIndex >= sizeof(sequencerSpeed)) {
+    speedIndex = 0;
+  }
 }
 
 void changeSequence() {
   sequenceIndex++;
 
-  if (sequenceIndex >= numberOfLights) {
+  if (sequenceIndex >= sizeof(sequencerData)) {
     sequenceIndex = 0;
   }
 }
@@ -30,5 +71,4 @@ void sequencer() {
       Serial.println(ii);
     }
   }
-
 }
